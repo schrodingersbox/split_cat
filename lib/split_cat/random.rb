5b523ca696d06a6588a4c58f024065ff013c7d8d
Rich Humphrey <rdh727@gmail.com>
2013-09-13 17:07:48 -0700

    Fixed problem with random data generator and specced it

--- lib/split_cat/random.rb
+++ lib/split_cat/random.rb
@@ -10,10 +10,7 @@ module SplitCat
       tokens = []
       (1..n_subjects).each do |i|
         tokens << SplitCat::Subject.create.token
-        puts "Token #{i} = #{tokens.last}"
       end
-
-      puts "Experiment #{i} = #{experiment.name}"
 
       (1..n_experiments).each do |i|
         experiment = Experiment.create( :name => "test_#{i}_#{Time.now.to_i}" )
@@ -32,8 +29,7 @@ module SplitCat
         experiment.save!
 
         tokens.each do |token|
-          puts "Hypothesis #{token}"
-          hypothesis = experiment.get_hypothesis( token )
+          experiment.get_hypothesis( token )
           experiment.goals.each do |goal|
             if rand > 0.5
               experiment.record_goal( goal.name, token )
--- spec/lib/split_cat/random_spec.rb
+++ spec/lib/split_cat/random_spec.rb
@@ -1,11 +1,39 @@
 require 'spec_helper'
 
+include SplitCat
+
 module SplitCat
+  module Random
 
-  describe Random do
+    describe '::generate' do
 
-    it 'should be specced'
+      before( :each ) do
+        @n_subjects = 10
+        @n_experiments = 2
+        @max_items = 5
+
+        @args = { :n_subjects => @n_subjects, :n_experiments => @n_experiments, :max_items => @max_items }
+      end
+
+      it 'generates the requested data' do
+        Subject.count.should eql( 0 )
+        Experiment.count.should eql( 0 )
+        Hypothesis.count.should eql( 0 )
+        Goal.count.should eql( 0 )
+        HypothesisSubject.count.should eql( 0 )
+        GoalSubject.count.should eql( 0 )
+
+        SplitCat::Random.generate( @args )
+
+        Subject.count.should eql( @n_subjects )
+        Experiment.count.should eql( @n_experiments )
+        Hypothesis.count.should > 0
+        Goal.count.should > 0
+        HypothesisSubject.count.should > 0
+        GoalSubject.count.should > 0
+      end
+
+    end
 
   end
-
 end
